964d9f983e1d59f1b6511c5aef88519d5ef0d960
Hz3r <fabbriziobarrios16@gmail.com>
2026-02-11 14:38:13 -0500

    inicializando backend

---
+++ backend/src/test/java/com/hzer/inventario_system/InventarioSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzer.inventario_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
